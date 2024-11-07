6aa12f683960a0bc20a5df0e45c40ad447bc9f67
dev8 <dev8.sofona@gmail.com>
2024-11-07 21:40:25 +0200

    +render service

---
+++ src/services/RenderService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace light\app\services;
+
+
+
+class RenderService
+{
+    public static function template(string $filename, array $attributes = []): void
+    {
+        if ($attributes) {
+            extract($attributes);
+        }
+
+        include(self::getPath($filename));
+    }
+
+
+    public static function get(string $filename, array $attributes = []): string
+    {
+        ob_start();
+
+        self::template($filename, $attributes);
+
+        return trim(ob_get_clean());
+    }
+
+
+    public static function exists(string $filename): bool
+    {
+        return file_exists(self::getPath($filename));
+    }
+
+
+    private static function getPath(string $filename): string
+    {
+        if (strstr($filename, '@')) {
+            $filename = AliasService::getAlias($filename);
+        }
+
+        return $filename . '.php';
+    }
+}
